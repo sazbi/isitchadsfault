c4b5d15330b0866c6a474db9b1c1e146c4a161d1
Andrew Pierson <andrew@andysqr.com>
2023-05-11 16:03:41 -0400

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.scss';
+import Cat from './Cat';
+import Overlay from './Overlay';
+
+function App() {
+  return (
+    <div className="App">
+      <Cat />
+      <Overlay />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Overlay.tsx
@@ -0,0 +1,13 @@
+import './App.scss'
+
+function Overlay() {
+  return(
+    <div className="overlay">
+      <div className="centered-content">
+        <h1>Yes</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Overlay;
